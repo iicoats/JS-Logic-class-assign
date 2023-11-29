7742270f811252fa1bb65d089aff9ea1a2cc795a
iicoats <145803389+iicoats@users.noreply.github.com>
2023-11-29 11:00:23 -0600

    working on assignment

--- assign_js/assignment.js
+++ assign_js/assignment.js
@@ -17,3 +17,31 @@ function clickHandler() {
 submitButton.addEventListener("click",clickHandler);
 
 
+
+function createQuestion(questionText){
+    const container = document.getElementById("container");
+    const questionDiv = document.createElement("div");
+    questionDiv.classList.add("question");
+
+    const questionP = document.createElement("p");
+    questionP.textContent = questionText;
+
+    questionDiv.appendChild(questionP);
+    // container.appendChild(questionDiv);
+
+    const userAnswer = prompt(questionText + " (yes/no) ");
+    const answerP = document.createElement("p");
+
+    if(userAnswer.toLowerCase() === "yes") {
+        answerP.textContent = "You answered Yes.";
+    }else if (userAnswer.toLowerCase() === "no");{
+    answerP.textContent = "You answered No.";
+} else {  
+    answerP.textContent = "Invalid Answer";
+}
+    questionDiv.appendChild(answerP);
+}
+
+createQuestion("Is JavaScript a Progamming Language?");
+
+
